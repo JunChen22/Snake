79d8b3484a76f0a6d116bd7fb60a911b37155570
ItsThatJUN <ccjh522@gmail.com>
2017-12-24 01:07:00 -0800

    Added some function "prototype"

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("hello world")
+from msvcrt import kbhit
+from random import randint
+from time import clock
+from time import sleep
+import pygame
+
+
+y = randint(1,6)
+x = clock()
+
+def update():
+    pass
+
+def food():
+    pass
+
+
+def controlLogic():
+    pass
+
+def kbinput():
+    pass
+
+def boarder():
+    pass
+
+def body():
+    pass
+
+def main():
+    pass
+
+
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.update()
+
+
+pygame.quit()
+quit()
