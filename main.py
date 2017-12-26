83ce3c3c57a44deb4cc15a3eb3aab0413cc94c57
ItsThatJUN <ccjh522@gmail.com>
2017-12-25 17:53:09 -0800

    Added some more code regarding to the control . It will now continue movement when no input is entered . And will not doing anything when theres no keypressed .

--- main.py
+++ main.py
@@ -6,120 +6,133 @@
 
 from random import randint
 import pygame
-from msvcrt import kbhit
-
+import sys
+from pygame.locals import *
 
 WIDTH = 800
 HEIGHT = 600
-DIR = {273: 'UP', 274: 'DOWN', 275:'RIGHT', 276: 'LEFT'}
+
+DIR = {'UP',  'DOWN','RIGHT','LEFT'}
+
+Reve_DIR = {'UP': ('UP','DOWN'),
+            'DOWN': ('UP','DOWN'),
+            'RIGHT': ('RIGHT','LEFT'),
+            'LEFT':('RIGHT','LEFT')}
+
+snakeX = 80
+snakeY = 140
+current_dire = None
+snakeLength = 1
+
+def main():
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    tittle = pygame.display.set_caption("Snakey")
+    snakeHead = pygame.image.load("image/snake.png")
+
+    while True:
+        play()
 
 
-class Snake():
+def update():
+    pygame.display.update()
 
-    snakeX = 80
-    snakeY = 140
-    current_dire = None
-    snakeLength = 1
+#spawn a food for the snake
+def food():
+    pass
 
-    def __init__(self):
-
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        self.window = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.tittle = pygame.display.set_caption("Snakey")
-        self.snakeHead = pygame.image.load("image/snake.png")
+#sets the snake control logic
+### def continue_movement():
+###       dir[current_dire]
 
 
-    def update():
+### 1  notthing pressed , not going
+### 2  pressed goes one direction and all direction with only head
+### 3 pressed goes one direction and 2 direicont only
+### 4  goes and only goes one direcion if not pressed
+###
+def control(input):
+
+    if snakeLength > 1:
+        movement(input)
+    elif snakeLength == 1:
+        if  current_dire == "UP":
+            move_up()
+        if  current_dire == "DOWN":
+            move_down()
+        if  current_dire == "RIGHT":
+            move_left()
+        if  current_dire == "LEFT":
+            move_right()
+
+#determining where to move
+# no same direction or reverse direction
+def movement(input):
+
+    if input == K_UP and current_dire not in Reve_DIR:
+        move_up()
+    elif input == K_DOWN and current_dire not in Reve_DIR:
+        move_down()
+    elif input == K_RIGHT and current_dire not in Reve_DIR:
+        move_right()
+    elif input == K_LEFT and current_dire not in Reve_DIR:
+        move_left()
+    else:
+        pass
+
+def move_up():
+    current_dire = "UP"
+    snakeY-=1
+
+def move_down():
+    current_dire = "DOWN"
+    snakeY+= 1
+
+def move_right():
+    current_dire = "RIGHT"
+    snakeX+= 1
+
+def move_left():
+    current_dire = "LEFT"
+    snakeX-= 1
+
+
+
+def display():
+    window.fill((255, 255, 255))
+    window.blit(snakeHead, (snakeX,snakeY))
+
+#snake body
+def body():
+    pass
+
+#the loop of main control
+def play():
+
+    while True:
+        display()
+        #will fall out of this for loop if no key pressed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+            elif event.type == KEYDOWN:
+                control(event.key)
+
+            print("something")
         pygame.display.update()
+        clock.tick(60)
 
-    #spawn a food for the snake
-    def food():
-        pass
+#snaked died , new game
+def newGame():
+    pass
 
-    #sets the snake control logic
-    def control(self,input):
-
-        if input not in dir:
-            pass
-        elif self.snakeLength >1:
-            self.movement(self.dir[input])
-        else :#goes wheere ever wanted
-            if  self.current_dire == "UP":
-                self.snakeY -= 10
-            if  self.current_dire == "DOWN":
-                self.snakeY += 10
-            if  self.current_dire == "RIGHT":
-                self.snakeX += 10
-            if  self.current_dire == "LEFT":
-                self.snakeX -= 10
-            else:
-
-
-   ### def continue_movement(self):
- ###       dir[self.current_dire]
-###
-    ###   ### 1  notthing pressed , not going
- ###   2  pressed goes one direction and all direction with only head
-   ###  3 pressed goes one direction and 2 direicont only
-    ### 4  goes and only goes one direcion if not pressed
-###
-
-    def movement(self,input):
-
-        if input == "UP":
-            if self.current_dire != (dir[0] and dir[1]):
-                self.current_dire = "UP"
-        if input == "DOWN":
-            if self.current_dire != (dir[0] and dir[1]):
-                self.current_dire = "DOWN"
-        if input == "RIGHT":
-            if self.current_dire != (dir[2] and dir[3]):
-                self.current_dire = "RIGHT"
-        if input == "LEFT:
-            if self.current_dire != (dir[2] and dir[3]):
-                self.current_dire = "LEFT"
-
-    def display(self):
-        self.window.fill((255, 255, 255))
-        self.window.blit(self.snakeHead, (self.snakeX,self.snakeY))
-
-    #snake body
-    def body():
-        pass
-
-    #the loop of main control
-    def play(self):
-
-        while True:
-            self.display()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.quitgame()
-
-                if event.type == pygame.KEYDOWN:
-                    key = event.key
-                    self.control(key)
-                elif not kbhit():
-                    self.control(self.current_dire)
-
-                print("something")
-            pygame.display.update()
-            self.clock.tick(60)
-
-    #snaked died , new game
-    def newGame(self):
-        pass
-
-    def quitgame():
-        pygame.quit()
-        quit()
-
+def quitgame():
+    pygame.quit()
+    quit()
 
 if __name__ == "__main__":
-
-    game = Snake()
-    game.play()
+    main()
 
 
